967a403f373adb640384d71a54e90430632b0015
Masood Acheampong <masoodacheampong@gmail.com>
2023-09-12 17:02:51 +0100

    Added the rest of needed images and uuid.

--- admin-dashboard-app/src/assets/data/data.js
+++ admin-dashboard-app/src/assets/data/data.js
@@ -1,3 +1,5 @@
+// General Icons
+import uuid from "react-uuid";
 // Icon Imports
 /*Note the names of the Icons work like storing a picture inside a variable*/
 import dashboardIcon from "../svgs/round-space-dashboard.svg";
@@ -11,7 +13,12 @@ import saveIcon from "../svgs/save-icon.svg";
 import stockIcon from "../svgs/stock-icon.svg";
 import salesIcon from "../svgs/sales-icon.svg";
 import jobIcon from "../svgs/job-icon.svg";
-
+import uxIcon from "../images/ux-image.svg";
+import bugFixIcon from "../images/bug-fix-image.svg";
+import wordPressIcon from "../images/word-press-image.svg";
+import mobileDesignIcon from "../images/mobile-design.svg";
+import webDevIcon from "../images/web-dev-image.svg";
+import uiIcon from "../images/ui-desgining.svg";
 // Navbar data
 export const menuData = [
   {
@@ -72,5 +79,57 @@ export const topAnalyticsData = [
     text: "Booked Services",
     number: "12+",
     icon: jobIcon,
+  },
+];
+
+// Recent Orders data
+export const recentOrdersData = [
+  {
+    orderId: uuid(),
+    image: uxIcon,
+    serviceName: "Ui/Ux Desgining",
+    price: "$178",
+    totalOrder: "325",
+    totalAmount: "$1,46,660",
+  },
+  {
+    orderId: uuid(),
+    image: bugFixIcon,
+    serviceName: "Bug Fix",
+    price: "$14",
+    totalOrder: "53",
+    totalAmount: "$46,660",
+  },
+  {
+    orderId: uuid(),
+    image: wordPressIcon,
+    serviceName: "Wordpress",
+    price: "$21",
+    totalOrder: "78",
+    totalAmount: "$3,46,676",
+  },
+  {
+    orderId: uuid(),
+    image: mobileDesignIcon,
+    serviceName: "MobileDesign",
+    price: "$32",
+    totalOrder: "98",
+    totalAmount: "$3,46,981",
+  },
+];
+
+// Top Selling Services
+export const topSellingServices = [
+  {
+    image: webDevIcon,
+    text: "Web Developement",
+    stars: "5",
+    price: "$87",
+  },
+  {
+    image: uiIcon,
+    text: "UI/UX Development",
+    stars: "3",
+    price: "$987",
   },
 ];
